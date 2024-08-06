028249cb5418fd169b6a66e4241bb30fc2a33fcc
NITHYANANTHAN NAGARAJAN <112227969+nithiritgithup@users.noreply.github.com>
2024-08-06 16:45:53 +0530

    Update com.sh

--- com.sh
+++ com.sh
@@ -1,49 +1,50 @@
-echo "Enter you name:"
+#!/bin/bash
+
+echo "Enter your name:"
 read name
-echo "Welcome user ,"$name
-if [ $name == "vk" ]
-then
-echo "you are not allowed for this app"
+echo "Welcome user, $name"
+
+if [ "$name" == "vk" ]; then
+    echo "You are not allowed to use this app."
 else
-while true;
-do
-echo "1.make dir 2.make  file 3.remove dir and remove file 4.htop 5.tree 6.exit"
-echo "Enetr opt:"
-read opt
-if [ $opt -eq 1 ]
-then
-echo "Enter dir name:"
-read dir
-mkdir $dir
-echo "dir created succesfully"
-elif [ $opt -eq 2 ]
-then
-echo "enter file name:"
-read file
-touch file
-echo "file created successfully"
-elif  [ $opt -eq 3 ]
-then
-echo "Enter dir for remove:"
-read rs
-rmdir $rs
-echo "**********************dir removed succesfully********************************************"
-echo "Enter file name to remove:"
-read rs1
-rm rs1
-echo "file removed successfully"
-elif [ $opt -eq 4 ]
-then
-htop
-elif [ $opt -eq 5 ]
-then
-tree
-elif [ $opt -eq 6 ]
-then
-echo "Thank you visit our app"
-exit 0
-else
-echo "you enter wrong check once"
-fi
-done
+    while true; do
+        echo "1. Make directory"
+        echo "2. Make file"
+        echo "3. Remove directory and file"
+        echo "4. htop"
+        echo "5. tree"
+        echo "6. Exit"
+        echo "Enter option:"
+        read opt
+
+        if [ "$opt" -eq 1 ]; then
+            echo "Enter directory name:"
+            read dir
+            mkdir "$dir"
+            echo "Directory created successfully."
+        elif [ "$opt" -eq 2 ]; then
+            echo "Enter file name:"
+            read file
+            touch "$file"
+            echo "File created successfully."
+        elif [ "$opt" -eq 3 ]; then
+            echo "Enter directory to remove:"
+            read dir_to_remove
+            rmdir "$dir_to_remove"
+            echo "Directory removed successfully."
+            echo "Enter file name to remove:"
+            read file_to_remove
+            rm "$file_to_remove"
+            echo "File removed successfully."
+        elif [ "$opt" -eq 4 ]; then
+            htop
+        elif [ "$opt" -eq 5 ]; then
+            tree
+        elif [ "$opt" -eq 6 ]; then
+            echo "Thank you for visiting our app."
+            exit 0
+        else
+            echo "You entered an invalid option. Please check and try again."
+        fi
+    done
 fi
